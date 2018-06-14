3e3bbcf139515030845781b348b4c7c9181a5702
unknown <ehurst01@gmail.com>
2018-06-13 23:17:42 -0700

    Added beer background

--- src/components/Container/Container.js
+++ src/components/Container/Container.js
@@ -1,8 +1,14 @@
 import React from "react";
+import './Container.css'
 
+import Background from './beerBG.jpg'
+
+const bgImg = {
+    backgroundImage:`url(${Background}`
+}
 
 let Container = props => (
-    <div className="containter">
+    <div className="containter" style={bgImg}>
         {props.children}
     </div>
 )
